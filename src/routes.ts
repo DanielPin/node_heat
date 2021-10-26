5ae0cef02bb6688b1d550006ea71b2b1c5a81a91
Daniel <daniel.pin@outlook.com>
2021-10-25 23:41:28 -0300

    Pegar 3 ultimas mensagens

--- src/routes.ts
+++ src/routes.ts
@@ -1,11 +1,13 @@
 import { Router } from "express";
 import { AuthenticateUserController } from "./controllers/AuthenticateUserController";
 import { CreateMessageController } from "./controllers/CreateMessageController";
+import { GetLast3MessagesControlller } from "./controllers/GetLast3MessagesControlller";
 import { ensureAuthenticated } from "./middleware/ensureAuthenticated";
 
 const router = Router();
 
 router.post("/authenticate", new AuthenticateUserController().handle);
 router.post("/messages", ensureAuthenticated, new CreateMessageController().handle);
+router.get("/messages/last3", new GetLast3MessagesControlller().handle);
 
 export { router };
